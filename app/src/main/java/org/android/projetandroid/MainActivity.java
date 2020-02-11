0dca7a406d52bac9c1e03d371c4a834ddd5d3a5d
yassine <yassine.karami@outlook.com>
2020-02-11 22:18:18 +0100

    creation list activity

--- app/src/main/java/org/android/projetandroid/MainActivity.java
+++ app/src/main/java/org/android/projetandroid/MainActivity.java
@@ -2,6 +2,7 @@ package org.android.projetandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import butterknife.ButterKnife;
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.btnListActivity)
     public void clickedOnSwitchList() {
-
+        Intent intent = new Intent(this, ListActivity.class);
+        startActivity(intent);
     }
 }
