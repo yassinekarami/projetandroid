2f754567dac1b3aeb1e9473dc676b2d5a66df1ad
yassine <yassine.karami@outlook.com>
2020-02-11 22:08:11 +0100

    init project

---
+++ app/src/main/java/org/android/projetandroid/MainActivity.java
@@ -0,0 +1,23 @@
+package org.android.projetandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnListActivity)
+    public void clickedOnSwitchList() {
+
+    }
+}
