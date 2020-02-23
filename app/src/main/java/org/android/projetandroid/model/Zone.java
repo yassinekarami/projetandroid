e5ecf8f743d8491f9100e028899e05f92d61a442
yassine <yassine.karami@outlook.com>
2020-02-23 16:59:02 +0100

    creation model active android

--- app/src/main/java/org/android/projetandroid/model/Zone.java
+++ app/src/main/java/org/android/projetandroid/model/Zone.java
@@ -1,22 +1,34 @@
 package org.android.projetandroid.model;
 
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
 import com.google.gson.annotations.Expose;
 
-public class Zone {
+@Table(name = "Zones")
+public class Zone extends Model {
+
+    @Column(name="label", index = true, unique = true, onUniqueConflict = Column.ConflictAction.REPLACE)
+    public String label;
 
     @Expose
+    @Column(name = "country")
     private String country;
 
     @Expose
+    @Column(name = "name")
     private String name;
 
     @Expose
+    @Column(name = "city")
     private String city;
 
     @Expose
+    @Column(name = "count")
     private int count;
 
     @Expose
+    @Column(name = "location")
     private int location;
 
 
